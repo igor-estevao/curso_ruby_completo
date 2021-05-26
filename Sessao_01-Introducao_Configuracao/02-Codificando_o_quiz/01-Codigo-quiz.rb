8e364c949e4f96b4ac5b07f49a4aa3465d50d247
= <=>
2021-05-26 20:40:48 +0000

    Finalizando Sessao-01

---
+++ Sessao_01-Introducao_Configuracao/02-Codificando_o_quiz/01-Codigo-quiz.rb
@@ -0,0 +1,11 @@
+# codificando o que viu no quiz
+# o ruby é uma linguagem completa, similar ao python em semântica
+
+# no quiz foi visto um array e ele foi revertido com a função .reverse!
+
+address = [1,2,3,4]
+
+p address
+
+new_address = address.reverse!
+p new_address
